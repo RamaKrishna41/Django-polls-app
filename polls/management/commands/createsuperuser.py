77513bcf1fc0e23089c71ecef637486128fdcbd5
RamaKrishna41 <ramakrishna665577@gmail.com>
2024-08-07 01:41:45 -0400

    Updating Requirements

--- polls/management/commands/createsuperuser.py
+++ polls/management/commands/createsuperuser.py
@@ -11,8 +11,14 @@ class Command(BaseCommand):
         email = os.getenv('DJANGO_SUPERUSER_EMAIL')
         password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f'Superuser {username} created'))
-        else:
-            self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
+        try:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+                self.stdout.write(self.style.SUCCESS(f'Superuser {username} created'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
+        except IntegrityError as e:
+            self.stdout.write(self.style.ERROR(f'IntegrityError: {e}'))
+            pass
+        except Exception as e:
+            pass
